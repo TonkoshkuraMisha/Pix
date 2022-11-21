b06055f4d55107757abf8ff6d9702a924fd912ea
tonkoshkura.misha <tonkoshkura.misha@gmail.com>
2022-11-21 22:45:20 +0200

    images was added

--- ship.py
+++ ship.py
@@ -31,9 +31,9 @@ class Ship():
 
     def update(self):
         # Обновляется атрибут x, не rect.
-        if self.moving_right and self.rect.right + 300 < self.screen_rect.right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left and self.rect.left > 300:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         # Обновление атрибута rect на основании self.x.
         self.rect.x = self.x
