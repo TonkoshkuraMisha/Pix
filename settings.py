35f8ab67abfa212e56fa8ce18dc139fee6ba1c1a
TonkoshkuraMisha <tonkoshkura.misha@google.com>
2023-02-18 16:02:17 +0200

    hight score

--- settings.py
+++ settings.py
@@ -32,6 +32,8 @@ class Settings():
 
         # Темп ускорения игры
         self.speedup_scale = 1.1
+        # Темп роста стоимости пришельцев
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -52,3 +54,4 @@ class Settings():
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
